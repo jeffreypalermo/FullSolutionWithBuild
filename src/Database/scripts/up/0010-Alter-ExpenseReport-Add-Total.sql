b97cdc5806a6471488c5f6855d2fe3d8f5ad2099
Colin Detwiller <colinsr@me.com>
2015-09-21 13:09:08 -0400

    removing unnecessary directories.

--- src/Database/scripts/up/0010-Alter-ExpenseReport-Add-Total.sql
+++
@@ -1,3 +0,0 @@
-
-ALTER TABLE dbo.ExpenseReport
-ADD [Total] MONEY
--- src/Database/scripts/up/0011-Create-ExpenseReportFact-Table.sql
+++
@@ -1,3 +0,0 @@
-﻿create table ExpenseReportFact(
-Id uniqueIdentifier primary key, Number varChar(5), TimeStamp DateTime, Total money, Status varChar(200), Submitter varChar(200), Approver varChar(200))
- 
