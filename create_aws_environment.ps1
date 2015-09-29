b289a2bb263c5db3aa3694fed9233654eb26da4f
Colin Detwiller <colinsr@me.com>
2015-09-29 16:18:13 -0400

    trying aws cli again

--- create_aws_environment.ps1
+++ create_aws_environment.ps1
@@ -20,15 +20,19 @@ $env:path += ";C:\Program Files\Amazon\AWSCLI\"
 #   Write-Host "Still creating stack"
 # }
 
-# $initial_state = & aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE 
-# aws cloudformation create-stack --stack-name ClearMeasureBootcamp --template-body file:///src/AWS/BootCamp.template --parameters file:///src/AWS/cf_parameters.json
-# 
-# do {
-#  $stack = & aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE
-#  Start-Sleep -s 60
-#  Write-Host "Still creating stack"
-# } while ($stack -eq $initial_state)
+ $initial_state = & aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE 
+ 
+ aws cloudformation create-stack --stack-name ClearMeasureBootcamp --template-body https://s3.amazonaws.com/cm-projectbootcamp/cloud_formation/BootCamp.template --parameters file:///src/AWS/cf_parameters.json
+ 
+ do {
+  $stack = & aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE
+  Write-Host $initial_state
 
-New-CFNStack -StackName "ClearMeasureBootcamp" `
-             -TemplateURL https://s3.amazonaws.com/cm-projectbootcamp/cloud_formation/BootCamp.template `
-             -Parameter @{ ParameterKey="AdminPassword"; ParameterValue="password123" } `
+  Write-Host $stack
+  Start-Sleep -s 60
+  Write-Host "Still creating stack"
+ } while ($stack -eq $initial_state)
+
+# New-CFNStack -StackName "ClearMeasureBootcamp" `
+#              -TemplateURL https://s3.amazonaws.com/cm-projectbootcamp/cloud_formation/BootCamp.template `
+#              -Parameter @{ ParameterKey="AdminPassword"; ParameterValue="password123" } `
