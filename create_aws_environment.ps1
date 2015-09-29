336854997267e1ffc35363aa1a15c8952b8af078
Colin Detwiller <colinsr@me.com>
2015-09-29 15:50:30 -0400

    trying AWS PowerShell Tools

--- create_aws_environment.ps1
+++ create_aws_environment.ps1
@@ -20,11 +20,15 @@ $env:path += ";C:\Program Files\Amazon\AWSCLI\"
 #   Write-Host "Still creating stack"
 # }
 
-$initial_state = & aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE 
-aws cloudformation create-stack --stack-name ClearMeasureBootcamp --template-body file:///src/AWS/BootCamp.template --parameters file:///src/AWS/cf_parameters.json
+# $initial_state = & aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE 
+# aws cloudformation create-stack --stack-name ClearMeasureBootcamp --template-body file:///src/AWS/BootCamp.template --parameters file:///src/AWS/cf_parameters.json
+# 
+# do {
+#  $stack = & aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE
+#  Start-Sleep -s 60
+#  Write-Host "Still creating stack"
+# } while ($stack -eq $initial_state)
 
-do {
- $stack = & aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE
- Start-Sleep -s 60
- Write-Host "Still creating stack"
-} while ($stack -eq $initial_state)
+New-CFNStack -StackName "ClearMeasureBootcamp" `
+             -TemplateURL https://s3.amazonaws.com/cm-projectbootcamp/cloud_formation/BootCamp.template `
+             -Parameter @{ ParameterKey="AdminPassword"; ParameterValue="password123" } `
