225c88d17b547d8ccb84ea66dcb1370db519f181
Colin Detwiller <colinsr@me.com>
2015-09-29 12:47:23 -0400

    tweak aws creation script again

--- create_aws_environment.ps1
+++ create_aws_environment.ps1
@@ -1,17 +1,25 @@
-function Create-Stack
-{
-	aws cloudformation create-stack --stackname ClearMeasureBootcamp --template-body file:///src/AWS/BootCamp.template --parameters file:///src/AWS/cf_parameters.json
-}
+# function Create-Stack
+# {
+# 	aws cloudformation create-stack --stackname ClearMeasureBootcamp --template-body file:///src/AWS/BootCamp.template --parameters file:///src/AWS/cf_parameters.json
+# }
+# 
+# function Get-Stack-Creation-Status
+# {
+# 	aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE #get status of CF stack
+# }
+# 
+# Create-Stack
 
-function Get-Stack-Creation-Status
-{
-	aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE #get status of CF stack
-}
+# $stack = aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE
+# 
+# while($stack -ne "CREATE_COMPLETE")
+# {
+#   Start-Sleep -s 60
+#   Write-Host "Still creating stack"
+# }
 
-Create-Stack
-
-while(Get-Stack-Creation-Status -ne "CREATE_COMPLETE")
-{
+do {
+  $stack = aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE
   Start-Sleep -s 60
   Write-Host "Still creating stack"
-}
+} while (!($stack))
