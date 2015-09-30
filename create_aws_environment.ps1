89256647db3d83865b13de3617a9df705181c8e5
Colin Detwiller <colinsr@me.com>
2015-09-30 12:17:20 -0400

    fixing up cloudformation kickoff script

--- create_aws_environment.ps1
+++ create_aws_environment.ps1
@@ -1,38 +1,37 @@
 $env:path += ";C:\Program Files\Amazon\AWSCLI\"
+$stack_name = "ClearMeasureBootcamp"
+$creation_complete = "CREATE_COMPLETE"
+$rollback_complete = "ROLLBACK_COMPLETE"
+$creation_in_progress "CREATE_IN_PROGRESS"
+$aws_region = "us-east-1"
+$template_body_url = https://s3.amazonaws.com/cm-projectbootcamp/cloud_formation/BootCamp.template
+$parameters_url = https://s3.amazonaws.com/cm-projectbootcamp/cloud_formation/cf_parameters.json
 
-# function Create-Stack
-# {
-# 	aws cloudformation create-stack --stackname ClearMeasureBootcamp --template-body file:///src/AWS/BootCamp.template --parameters file:///src/AWS/cf_parameters.json
-# }
-# 
-# function Get-Stack-Creation-Status
-# {
-# 	aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE #get status of CF stack
-# }
-# 
-# Create-Stack
+function CreateStack {
+  aws cloudformation create-stack --region $aws_region --stack-name $stack_name --template-body $template_body_url --parameters $parameters_url
+}
 
-# $stack = aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE
-# 
-# while($stack -ne "CREATE_COMPLETE")
-# {
-#   Start-Sleep -s 60
-#   Write-Host "Still creating stack"
-# }
+function DeleteStack {
+  aws cloudformation delete-stack --stack-name $stack_name
+}
 
- $initial_state = & aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE 
- 
- aws cloudformation create-stack --region "us-east-1" --stack-name "ClearMeasureBootcamp" --template-body https://s3.amazonaws.com/cm-projectbootcamp/cloud_formation/BootCamp.template --parameters https://s3.amazonaws.com/cm-projectbootcamp/cloud_formation/cf_parameters.json 
+CreateStack 
  
  do {
-  $stack = & aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE
-  Write-Host $initial_state
-
-  Write-Host $stack
-  Start-Sleep -s 60
-  Write-Host "Still creating stack"
- } while ($stack -eq $initial_state)
-
-# New-CFNStack -StackName "ClearMeasureBootcamp" `
-#              -TemplateURL https://s3.amazonaws.com/cm-projectbootcamp/cloud_formation/BootCamp.template `
-#              -Parameter @{ ParameterKey="AdminPassword"; ParameterValue="password123" } `
+  $all_stacks = aws cloudformation list-stacks | ConvertFrom-JSON
+  
+  $bootcamp_stack = $all_stacks.StackSummaries | ? { $_.StackName -eq $stack-name } Select -First 1
+    
+  $current_status = $bootcamp_stack.StackStatus
+  
+  If ($current_status -eq $creation_complete) { break }
+  
+  If ($current_status -eq $rollback_complete) {  
+    DeleteStack
+	CreateStack
+  }
+  Else {
+	Start-Sleep -s 60
+    Write-Host "Still creating stack"
+  }
+} while ($current_status -eq $creation_in_progress)
