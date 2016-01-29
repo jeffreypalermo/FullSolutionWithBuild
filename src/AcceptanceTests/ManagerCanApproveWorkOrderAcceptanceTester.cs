71fdaf7732c9583df172af79b7931918df59cf46
Jeffrey Palermo <jeffrey@clear-measure.com>
2016-01-29 10:04:28 -0600

    - Added acceptance tests project.  Couldn't wire everything up because I don't have internet at the moment (I'm on a plane).

---
+++ src/AcceptanceTests/ManagerCanApproveWorkOrderAcceptanceTester.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace AcceptanceTests
+{
+    [TestFixture]
+    public class ManagerCanManageWorkOrderAcceptanceTester
+    {
+        [Test]
+        public void ManagerCanApproveAWorkOrder()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void ManagerCanRejectAWorkOrder()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void ManagerCanReassignAWorkOrder()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
